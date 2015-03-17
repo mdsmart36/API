a5c50fb26001e58d6fad903bef4ead890f9fde0d
Matthew Smart <mdsmart36@gmail.com>
2015-03-17 13:27:23 -0500

    added default zip code functionality

--- js/main.js
+++ js/main.js
@@ -46,22 +46,46 @@ $(function () {
 				console.log(data);
 				var jsonObj = data.current_observation;
 				var time = jsonObj.local_time_rfc822;
-				var offset = data.current_observation.local_tz_offset.length;
+				var offset = jsonObj.local_tz_offset.length;
 
 				// set the text with the appropriate data
 				$('#date-time').text(time.slice(0, offset * (-1)));
 				$('#location').text(jsonObj.display_location.full + "  " + zip);
-				$('#temperature-string').text(jsonObj.temperature_string);
+				$('#temperature-string').text(jsonObj.temp_f + " degrees F");
 				$('#temperature-icon').attr('src', jsonObj.icon_url);
 			}
 		});
 	}
 
+	function getIpAddress() {
+		// use a synchronous request; will not work as asynchronous
+		var xmlHttp = new XMLHttpRequest();
+		
+		// GET data, from URL, false = synchronous request
+	    xmlHttp.open( "GET", "http://ip4.telize.com", false );
+	    xmlHttp.send(); // send the request
+
+	    // ip address is returned as text
+	    return xmlHttp.responseText; 
+	}
+
+	function getZipfromIP(ipAddress) {
+		var xmlHttp = new XMLHttpRequest();
+	    xmlHttp.open( "GET", "https://freegeoip.net/json/" + ipAddress, false );
+	    xmlHttp.send();
+	    // response is a JSON string which must be converted to an object
+	    return (($.parseJSON(xmlHttp.responseText)).zip_code);
+	}
+
+	var ipAddress = getIpAddress();
+	var defaultZip = getZipfromIP(ipAddress);
+
 	var button = $('button');
-	var inputText = $('#zip-code');
+	var inputText = $('#zip-code').val(defaultZip);
 
 	function processForm() {
 		var zip = inputText.val();
+
 		getCurrentTemp(zip);
 		inputText.val('');
 		inputText.focus();
